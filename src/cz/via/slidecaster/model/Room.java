d0d9eee2b04c3ed47a8a69f40ee22292ee7e5ece
Martin Stajner <marstaj@seznam.cz>
2012-12-17 09:48:12 +0100

    Update ROOM objektu

--- src/cz/via/slidecaster/model/Room.java
+++ src/cz/via/slidecaster/model/Room.java
@@ -4,79 +4,36 @@ import java.io.Serializable;
 
 public class Room implements Serializable {
 
-    String created_at;
-    String id;
-    String name;
-    String password;
-    String photo_id;
-    String updated_at;
-    private int imageNumber;
+	int id;
+	String name;
+	boolean has_passwd;
 
-    public Room() {
-        imageNumber = 0;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public String getCreated_at() {
-        return created_at;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public void setCreated_at(String created_at) {
-        this.created_at = created_at;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public boolean isHas_passwd() {
+		return has_passwd;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setHas_passwd(boolean has_passwd) {
+		this.has_passwd = has_passwd;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public boolean isPasswordProtected() {
+		return has_passwd;
+	}
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isPasswordProtected() {
-        return !this.getPassword().equals("");
-    }
-
-    public String getPhoto_id() {
-        return photo_id;
-    }
-
-    public void setPhoto_id(String photo_id) {
-        this.photo_id = photo_id;
-    }
-
-    public String getUpdated_at() {
-        return updated_at;
-    }
-
-    public void setUpdated_at(String updated_at) {
-        this.updated_at = updated_at;
-    }
-
-    public void nextImage() {
-        imageNumber++;
-    }
-
-    public void prevImage() {
-        imageNumber--;
-    }
-
-    public int getImageNumber() {
-        return imageNumber;
-    }
 }
